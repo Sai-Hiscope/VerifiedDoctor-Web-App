a8e2036e358553d9e80858d27d76673549f2e934
PRAVEEN-PUVVALA <praveen.p@hiscopetech.com>
2025-01-21 18:10:23 +0530

    insurance page

--- src/components/insuranceFormPage1.jsx
+++ src/components/insuranceFormPage1.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 const FormPage1 = () => {
   return (
-    <div>
-      
+    <div className='insurancefrompage1-div'>
+      <div className='insurancefrompage1-div1'>
+        <h3>Choose your needs</h3>
+        <p>How much insurance cover you are looking for ?...</p>
+      </div>
+      <div>
+        <ul>
+          <li><p>Basic Sum Assured</p>
+          <input type="text"  placeholder='Type here' required/>
+          </li>
+          <li>
+          <p>Premium Paying Term</p>
+          <input type="text"  placeholder='Type here' required/>
+          </li>
+        </ul>
+      </div>
       <p className='pagecount'>“Start Your Journey to Protection Today!”
       “It’s easy, quick, and tailored for you-Start your protection journey now”</p>
     </div>

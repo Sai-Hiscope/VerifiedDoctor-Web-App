32b74e89281d2b44bfe521959155b88ef407df55
sandeepHiscope <sandeep.m@hiscopetech.com>
2025-03-25 18:36:58 +0530

    same as last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from "react";
-import "./App.css"
-import React from 'react'
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
 import Homepage from "./pages/Home";
 import DoctorVerification from "./pages/doctorVerificationpage";
 import FindDoctorPage from "./pages/findDoctorPage";
@@ -15,31 +11,30 @@ import DoctorRegisterPage from "./pages/doctorRegisterPage";
 import FounderPage from "./pages/ourFoundersPage";
 import HomeDeliveryMedicine from "./pages/demoPage";
 import Insurance from "./pages/insurancePage";
-import DoctorProfilePage  from "./pages/doctorProfilePage";
+import DoctorProfilePage from "./pages/doctorProfilePage";
 import MainInsurance from "./pages/mainInsurancePage";
-
-
-
-
+import MainHeader from "./components/header"
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/doctorVerificationpage" element={<DoctorVerification />} />
-        <Route path="/findDoctorPage" element={<FindDoctorPage />} />
-        <Route path="/loginPage" element={<Login />} />
-        <Route path="/individualRegisterPage" element={<IndividualRegisterPage />} />
-        <Route path="/sosPage" element={<SosPage />} />
-        <Route path="/doctorRegisterPage" element={<DoctorRegisterPage />} />
-        <Route path="/FounderPage" element={<FounderPage />} />
-        <Route path="/insurancePage" element={<Insurance />} />
-        <Route path="/demoPage" element={<HomeDeliveryMedicine />} />
-        <Route path="/doctorProfilePage" element={<DoctorProfilePage />} />
-        <Route path="/mainInsurancePage" element={<MainInsurance />} />
-      </Routes>
-    </Router>
-  );
-}
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Homepage />} />
+    //     <Route path="/doctorVerificationpage" element={<DoctorVerification />} />
+    //     <Route path="/findDoctorPage" element={<FindDoctorPage />} />
+    //     <Route path="/loginPage" element={<Login />} />
+    //     <Route path="/individualRegisterPage" element={<IndividualRegisterPage />} />
+    //     <Route path="/sosPage" element={<SosPage />} />
+    //     <Route path="/doctorRegisterPage" element={<DoctorRegisterPage />} />
+    //     <Route path="/FounderPage" element={<FounderPage />} />
+    //     <Route path="/insurancePage" element={<Insurance />} />
+    //     <Route path="/demoPage" element={<HomeDeliveryMedicine />} />
+    //     <Route path="/doctorProfilePage" element={<DoctorProfilePage />} />
+    //     <Route path="/mainInsurancePage" element={<MainInsurance />} />
+    //   </Routes>
+    // </Router>
+    <MainHeader />
 
-export default App
+  );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,75 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
-import Homepage from "./pages/Home";
-import DoctorVerification from "./pages/doctorVerificationpage";
-import FindDoctorPage from "./pages/findDoctorPage";
-import Login from "./pages/loginPage";
-import IndividualRegisterPage from "./pages/individualRegisterPage";
-import SosPage from "./pages/sosPage";
-import DoctorRegisterPage from "./pages/doctorRegisterPage";
-import FounderPage from "./pages/ourFoundersPage";
-import HomeDeliveryMedicine from "./pages/demoPage";
-import Insurance from "./pages/insurancePage";
-import DoctorProfilePage  from "./pages/doctorProfilePage";
-import MainInsurance from "./pages/mainInsurancePage";
+import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from "react";
+import App from "./App";
+import { createRoot } from "react-dom/client";
+import MainHeader from "./components/header";
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Homepage />,
-  },
-  {
-    path: "/doctorVerificationpage",
-    element: <DoctorVerification />,
-  },
-  {
-    path: "/findDoctorPage",
-    element: <FindDoctorPage />,
-  },
-  {
-    path: "/loginPage",
-    element: <Login />,
-  },
-  {
-    path: "/individualRegisterPage",
-    element: <IndividualRegisterPage />,
-  },
-  {
-    path: "/sosPage",
-    element: <SosPage />,
-  },
-  {
-    path: "/doctorRegisterPage",
-    element: <DoctorRegisterPage />,
-  },
-  {
-    path: "/FounderPage",
-    element: <FounderPage />,
-  },  
-  {
-    path : "/insurancePage",
-    element:<Insurance />,
-  },
-  {
-    path: "/demoPage",
-    element: <HomeDeliveryMedicine />,
-  },
-  {
-
-    path: "/doctorProfilePage",
-    element: <DoctorProfilePage />,
-   },
-    {
-    path:"/mainInsurancePage",
-    element:<MainInsurance/>,
-
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <MainHeader/>
+    </BrowserRouter>
+  </StrictMode>
 );
